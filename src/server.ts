c60a8cf499fbd02ccaf3c4a9e510997473b94463
Marcos Tomaz <marcosvto1@gmail.co>
2020-08-04 12:26:09 -0300

    add profile heroku

--- src/server.ts
+++ src/server.ts
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3333;
 
 app.listen(port,() => {
     console.log('START SERVE PORT 3333');
