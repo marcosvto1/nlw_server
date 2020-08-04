74667da03ff1dfe1d1d6f4a5bd8a314d845ef5ac
Marcos Tomaz <marcosvto1@gmail.co>
2020-08-04 12:45:57 -0300

    add attr engine yarn

--- src/server.ts
+++ src/server.ts
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-const port = process.env.PORT || 3333;
+const port: string|number  = process.env.PORT || 3333;
 
 app.listen(port,() => {
     console.log('START SERVE PORT 3333');
